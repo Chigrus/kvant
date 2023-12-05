d5f72bae3314595142c72b9c3d7eff3aa3d7b87e
unknown <chigrus@yandex.ru>
2023-12-05 23:54:06 +0300

    modify open graph editor

--- src/components/OpenGraphEditor.svelte
+++ src/components/OpenGraphEditor.svelte
@@ -18,7 +18,7 @@
 
 
 	let cropData:Object;
-	let cropSize = {width: 370, height: 240};
+	let cropSize = {width: 1200, height: 630};
 	
 
     function clickEditOgImage(){
@@ -76,7 +76,7 @@
 						zoom={1}
 						minZoom={0.8}
 						maxZoom={2}
-						aspect = {370/240}
+						aspect = {600/315}
 						zoomSpeed={0.1}
 						restrictPosition={false}
 						on:cropcomplete={(event) => cropData = event.detail.pixels}
